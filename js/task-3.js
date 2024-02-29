66411e4ada24fd45e01ecbe80c7fefcedfaf55b7
Plyampus <plyampus@gmail.com>
2024-02-29 20:08:03 +0200

    Update tasks: 1, 2, 3, 4, 5, 6.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,7 @@
-class StringBuilder {
-  #value;
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener('input', function () {
+    const trimmedValue = nameInput.value.trim();
+    nameOutput.textContent = trimmedValue || 'Anonymous';
+});
